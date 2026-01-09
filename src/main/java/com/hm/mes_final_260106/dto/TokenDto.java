0bd690b9287857c0fa033eb5528a91e4764012e7
jks2024 <jks2024@gmail.com>
2026-01-09 12:16:23 +0900

    JWT

---
+++ src/main/java/com/hm/mes_final_260106/dto/TokenDto.java
@@ -0,0 +1,14 @@
+package com.hm.mes_final_260106.dto;
+// 로그인 성공 시 응답으로 클라이언트에 전달
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Builder
+public class TokenDto {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private Long accessTokenExpiresIn;
+    private Long refreshExpiresIn;
+}
